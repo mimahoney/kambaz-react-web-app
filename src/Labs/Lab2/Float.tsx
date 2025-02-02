39065e02818e2fd0ccb15206e73d279bb8f9a179
mimahoney <mahoney.mich@northeastern.edu>
2025-02-02 16:34:28 -0500

    sunday changes

---
+++ src/Labs/Lab2/Float.tsx
@@ -0,0 +1,53 @@
+import "./index.css";
+
+
+const Float = () => {
+    return (
+<div id="wd-float-divs">
+ <h2>Float</h2>
+ <div>
+   <img className="wd-float-right"
+     src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"/>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tincidunt ut odio quis tincidunt. Ut tortor est, auctor eget mi non, facilisis scelerisque nulla. Mauris at tincidunt nisl. Suspendisse volutpat, est quis efficitur aliquet, est tortor dapibus dolor, eu tincidunt lectus ipsum in sapien. Nullam vel nisl nisi. Aliquam tortor purus, tempus id mattis in, consequat at est. Cras sit amet augue quis nulla ultrices malesuada. Aliquam tristique dapibus orci, imperdiet maximus magna imperdiet sed. Vestibulum at nisl urna. Donec viverra placerat pharetra. Sed sit amet risus tellus. Nulla blandit posuere mattis. In iaculis, enim eget cursus imperdiet, lectus tellus vehicula tortor, a ullamcorper mauris justo eu elit.
+
+Nullam sed ante eu dolor porttitor tristique. Pellentesque lacinia felis ut odio dictum rutrum. Suspendisse tincidunt nunc non sapien viverra dignissim. Duis malesuada mollis eros, eget sollicitudin ex fermentum at. Morbi fermentum nibh vitae volutpat rutrum. Vivamus tempor vel augue at porta. Etiam sollicitudin velit odio, et aliquet ex accumsan eu. Cras interdum velit ac sem dignissim consectetur. Proin fermentum vulputate imperdiet. Vivamus feugiat urna non purus porta, et elementum tellus egestas. Duis hendrerit turpis venenatis ex egestas, ut dignissim arcu gravida. Fusce egestas, odio et rutrum mollis, quam magna 
+   <img className="wd-float-left"
+     src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"/>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tincidunt ut odio quis tincidunt. Ut tortor est, auctor eget mi non, facilisis scelerisque nulla. Mauris at tincidunt nisl. Suspendisse volutpat, est quis efficitur aliquet, est tortor dapibus dolor, eu tincidunt lectus ipsum in sapien. Nullam vel nisl nisi. Aliquam tortor purus, tempus id mattis in, consequat at est. Cras sit amet augue quis nulla ultrices malesuada. Aliquam tristique dapibus orci, imperdiet maximus magna imperdiet sed. Vestibulum at nisl urna. Donec viverra placerat pharetra. Sed sit amet risus tellus. Nulla blandit posuere mattis. In iaculis, enim eget cursus imperdiet, lectus tellus vehicula tortor, a ullamcorper mauris justo eu elit.
+
+Nullam sed ante eu dolor porttitor tristique. Pellentesque lacinia felis ut odio dictum rutrum. Suspendisse tincidunt nunc non sapien viverra dignissim. Duis malesuada mollis eros, eget sollicitudin ex fermentum at. Morbi fermentum nibh vitae volutpat rutrum. Vivamus tempor vel augue at porta. Etiam sollicitudin velit odio, et aliquet ex accumsan eu. Cras interdum velit ac sem dignissim consectetur. Proin fermentum vulputate imperdiet. Vivamus feugiat urna non purus porta, et elementum tellus egestas. Duis hendrerit turpis venenatis ex egestas, ut dignissim arcu gravida. Fusce egestas, odio et rutrum mollis, quam magna 
+   <img className="wd-float-right"
+     src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"/>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tincidunt ut odio quis tincidunt. Ut tortor est, auctor eget mi non, facilisis scelerisque nulla. Mauris at tincidunt nisl. Suspendisse volutpat, est quis efficitur aliquet, est tortor dapibus dolor, eu tincidunt lectus ipsum in sapien. Nullam vel nisl nisi. Aliquam tortor purus, tempus id mattis in, consequat at est. Cras sit amet augue quis nulla ultrices malesuada. Aliquam tristique dapibus orci, imperdiet maximus magna imperdiet sed. Vestibulum at nisl urna. Donec viverra placerat pharetra. Sed sit amet risus tellus. Nulla blandit posuere mattis. In iaculis, enim eget cursus imperdiet, lectus tellus vehicula tortor, a ullamcorper mauris justo eu elit.
+
+Nullam sed ante eu dolor porttitor tristique. Pellentesque lacinia felis ut odio dictum rutrum. Suspendisse tincidunt nunc non sapien viverra dignissim. Duis malesuada mollis eros, eget sollicitudin ex fermentum at. Morbi fermentum nibh vitae volutpat rutrum. Vivamus tempor vel augue at porta. Etiam sollicitudin velit odio, et aliquet ex accumsan eu. Cras interdum velit ac sem dignissim consectetur. Proin fermentum vulputate imperdiet. Vivamus feugiat urna non purus porta, et elementum tellus egestas. Duis hendrerit turpis venenatis ex egestas, ut dignissim arcu gravida. Fusce egestas, odio et rutrum mollis, quam magna 
+   <img className="wd-float-left"
+     src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"/>
+   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tincidunt ut odio quis tincidunt. Ut tortor est, auctor eget mi non, facilisis scelerisque nulla. Mauris at tincidunt nisl. Suspendisse volutpat, est quis efficitur aliquet, est tortor dapibus dolor, eu tincidunt lectus ipsum in sapien. Nullam vel nisl nisi. Aliquam tortor purus, tempus id mattis in, consequat at est. Cras sit amet augue quis nulla ultrices malesuada. Aliquam tristique dapibus orci, imperdiet maximus magna imperdiet sed. Vestibulum at nisl urna. Donec viverra placerat pharetra. Sed sit amet risus tellus. Nulla blandit posuere mattis. In iaculis, enim eget cursus imperdiet, lectus tellus vehicula tortor, a ullamcorper mauris justo eu elit.
+
+Nullam sed ante eu dolor porttitor tristique. Pellentesque lacinia felis ut odio dictum rutrum. Suspendisse tincidunt nunc non sapien viverra dignissim. Duis malesuada mollis eros, eget sollicitudin ex fermentum at. Morbi fermentum nibh vitae volutpat rutrum. Vivamus tempor vel augue at porta. Etiam sollicitudin velit odio, et aliquet ex accumsan eu. Cras interdum velit ac sem dignissim consectetur. Proin fermentum vulputate imperdiet. Vivamus feugiat urna non purus porta, et elementum tellus egestas. Duis hendrerit turpis venenatis ex egestas, ut dignissim arcu gravida. Fusce egestas, odio et rutrum mollis, quam magna 
+   <div className="wd-float-done"></div>
+ </div>
+ <div id="wd-float-divs">
+ <h2>Float</h2>
+ <div>
+   <div className="wd-float-left wd-dimension-portrait wd-bg-color-yellow">
+     Yellow </div>
+   <div className="wd-float-left wd-dimension-portrait wd-bg-color-blue wd-fg-color-white">
+     Blue </div>
+   <div className="wd-float-left wd-dimension-portrait wd-bg-color-red">
+     Red </div>
+   <img className="wd-float-right"
+     src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"/>
+   <div className="wd-float-done"></div>
+ </div>
+</div>
+
+</div>
+
+
+
+    );
+  };
+  
+  export default Float;
