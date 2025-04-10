a9c3e494146f0f2e6b9067d07c59e09d27c0ade2
mimahoney <mahoney.mich@northeastern.edu>
2025-04-10 16:34:33 -0400

    Revert "Merge branch 'project' of https://github.com/mimahoney/kambaz-react-web-app into project"
    This reverts commit 3bbc59a99df6f1cbaf1abe6a18a6a54eefbb458d, reversing
    changes made to a200f6450e2862dc1402abb2e2d74ef9edc4a993.

--- src/Kambaz/Courses/Quizzes/index.tsx
+++ src/Kambaz/Courses/Quizzes/index.tsx
@@ -73,6 +73,7 @@ export default function Quizzes() {
           >
             + Group
           </Button>
+<<<<<<< Updated upstream
           <Button
             size="lg"
             className="me-1 float-end"
@@ -80,6 +81,9 @@ export default function Quizzes() {
             variant="danger"
             onClick={() => n(`/Kambaz/Courses/${cid}/Quizzes/new`)}
           >
+=======
+          <Button size="lg" className="me-1 float-end" id="wd-add-quiz" variant="danger" onClick={() => n(`/Kambaz/Courses/${cid}/Quizzes/new`)}>
+>>>>>>> Stashed changes
             + Quiz
           </Button>
         </div>
