deefcfe6260b24586ef39ad1abd475ea3b469ff3
mimahoney <mahoney.mich@northeastern.edu>
2025-04-16 09:18:25 -0400

    update client.ts

--- src/Kambaz/Account/client.ts
+++ src/Kambaz/Account/client.ts
@@ -66,11 +66,11 @@ export const findCoursesForUser = async (userId: string) => {
   return response.data;
 };
 export const enrollIntoCourse = async (userId: string, courseId: string) => {
-  const response = await axiosWithCredentials.post(`${USERS_API}/${userId}/courses/${courseId}`);
+  const response = await axiosWithCredentials.post(`${USERS_API}/${userId}/courses/${courseId}/enroll`);
   return response.data;
  };
  export const unenrollFromCourse = async (userId: string, courseId: string) => {
-  const response = await axiosWithCredentials.delete(`${USERS_API}/${userId}/courses/${courseId}`);
+  const response = await axiosWithCredentials.delete(`${USERS_API}/${userId}/courses/${courseId}/unenroll`);
   return response.data;
  };
  
